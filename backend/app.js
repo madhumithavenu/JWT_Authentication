4541e42d92c7731a6b2dbdcdd727b3d2b5afcc83
Madhumitha <madhumithavenu03@gmail.com>
2024-01-10 12:10:33 +0530

    connected to backend database

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('/',(req,res,next)=>{
+mongoose.connect("mongodb+srv://admin:JPEHjE4K51uMmpve@cluster0.tlp49tp.mongodb.net/auth?retryWrites=true&w=majority")
+    .then(() => {
+        app.listen(5000);
+        console.log('Database Connected! Listening to localhost 5000');
+    })
+    .catch(err => console.log(err))
+
+app.use('/', (req, res, next) => {
     res.send('Hello Here!')
 })
 
-app.listen(5000,()=>{
-    console.log('Listening to localhost 5000');
-})
+
+
+// JPEHjE4K51uMmpve
