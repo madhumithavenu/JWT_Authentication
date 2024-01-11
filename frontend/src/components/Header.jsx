94e97e0166ce5234cdfa4a540d5fd15d4e6014dd
Madhumitha <madhumithavenu03@gmail.com>
2024-01-11 10:53:55 +0530

    designing the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import {AppBar, Box, Tab, Tabs, Toolbar, Typography} from "@mui/material";
+import React, { useState } from 'react';
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 
 function Header() {
+  const [value,setValue] = useState();
   return (
     <div>
       <AppBar>
         <Toolbar>
           <Typography variant='h3'>MernAuth</Typography>
-          <Box>
-            <Tabs>
-              <Tab/>
+          <Box sx={{ marginLeft: 'auto' }}>
+            <Tabs indicatorColor='secondary'
+            onChange={(e,val)=>setValue(val)} 
+            value={value} 
+            textColor='inherit'>
+              <Tab label="Login" />
+              <Tab label="Signup" />
             </Tabs>
           </Box>
         </Toolbar>
