35637ea06f182ae961d6698373cf31e688fe609d
Madhumitha <madhumithavenu03@gmail.com>
2024-01-11 10:59:13 +0530

    adding routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.jsx';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+import Welcome from './components/Welcome.jsx';
 
 function App() {
   return (
-    <div>
+    <>
+    <header>
       <Header/>
-    </div>
+    </header>
+    <main>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/user' element={<Welcome/>}/>
+      </Routes>
+    </main>
+    </>
   );
 }
 
