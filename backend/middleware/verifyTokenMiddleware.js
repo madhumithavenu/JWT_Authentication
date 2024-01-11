e0ee410d6b1c21f443f9755db30aa161ea4e8a60
Madhumitha <madhumithavenu03@gmail.com>
2024-01-11 10:39:41 +0530

    using httpOnly cookie feature

--- backend/middleware/verifyTokenMiddleware.js
+++ backend/middleware/verifyTokenMiddleware.js
@@ -1,25 +1,26 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
-dotenv.config({path: '../.env'});
+dotenv.config({ path: '../.env' });
 
-function verifyToken(req, res, next){
+function verifyToken(req, res, next) {
     // const headers = req.headers[`authorization`];
     // const token = headers.split(" ")[1];
-    // if(!token){
-    //     return res.status(404).json({message: "No token found"});
-    // }
-    // jwt.verify(String(token),process.env.JWT_SECRET_KEY,(err,user)=>{
-    //     if(err){
-    //       return res.status(400).json({message: "Invalid token"});
-    //     }
-    //     console.log(user);
-    //     req._id = user._id;
-    // });
-    // next();
 
     const cookies = req.headers.cookie;
-    console.log(cookies);
+    if (!cookies) {
+        return res.status(404).json({ message: "No token found" });
+    }
+
+    const token = cookies.split('=')[1];
+    jwt.verify(String(token), process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(400).json({ message: "Invalid token" });
+        }
+        console.log(user);
+        req._id = user._id;
+    });
+    next();
 };
 
 exports.verifyToken = verifyToken;
